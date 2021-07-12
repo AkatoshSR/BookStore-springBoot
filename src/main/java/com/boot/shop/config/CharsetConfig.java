3de8b5408ca07fae76219c252ee00b479b67c2b4
AkatoshSR <sqx2211@qq.com>
2021-07-12 09:49:40 +0800

    second

---
+++ src/main/java/com/boot/shop/config/CharsetConfig.java
@@ -0,0 +1,11 @@
+package com.boot.shop.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CharsetConfig implements WebMvcConfigurer {
+
+
+
+}
--- src/main/java/com/boot/shop/controller/UserController.java
+++ src/main/java/com/boot/shop/controller/UserController.java
@@ -6,18 +6,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
-public class UserController {
+public class UserController extends BaseController{
 
     /*
         重定向：redirect:/地址.html
             1.浏览器地址栏地址变化
             2.可以访问到static文件夹页面
             3.不可以访问到templates
+            4.可以找到static文件和controller里面的地址
+            5.重定向传参，xxx?a=value1&b=value2
+
         转发 /地址
             1.浏览器地址栏不变
             2.可以访问templates文件夹页面
-            3.布恩那个访问static
+            3.不能访问static
+            4.转发传参：req.setAttribute('a', value)
 
      */
 
@@ -29,17 +35,18 @@ public class UserController {
     @RequestMapping("/login")
     public String login(String username,String password){
         UserBean user = userMapper.getUser(username, password);
-        if (user != null){
-            System.out.println("登录成功");
-            return "/main";
-        }else{
 
-            return "redirect:/index.html"; // 重定向
-        }
+        return user != null ? ("redirect:/main?uid=" + user.getId()) :
+                ("redirect:/index.html?msg=" + getUTF8Param("用户名或密码错误"));
     }
 
     @RequestMapping("/main")
-    public String main(){
+    public String main(int uid, HttpServletRequest request){
+        System.out.println(uid);
+        // selectById是BaseMapper封装的函数，根据id查询
+        UserBean bean = userMapper.selectById(uid);
+        String username = bean.getUsername();
+        request.setAttribute("bean", userMapper.selectById(uid));
         return "/main";
     }
 
